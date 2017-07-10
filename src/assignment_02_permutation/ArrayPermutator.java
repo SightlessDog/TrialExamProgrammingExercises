cd5637a381e76858116cc18188ae616c720d2298
B Kleinen <kleinen@infrastructure.de>
2017-07-10 22:40:27 +0200

    assignment

---
+++ src/assignment_02_permutation/ArrayPermutator.java
@@ -0,0 +1,27 @@
+package assignment_02_permutation;
+
+import java.util.Random;
+
+/**
+ * Assignment 02: Implement the method permutate that
+ * permutates the array given as a parameter in place.
+ * @author kleinen
+ *
+ * @param <E>
+ */
+public class ArrayPermutator<E> {
+	// don't create a new Instance of Random - use 
+	// the one passed by the test! 
+	Random random;
+	public ArrayPermutator(Random randomGenerator){
+		random = randomGenerator;
+	}
+	
+	/**
+	 * Permutates the array in place.
+	 * @param a
+	 */
+	public void permutate(E[] a){
+		// implement the method!
+	}
+}
