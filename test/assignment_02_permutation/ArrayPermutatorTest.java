1aa49d62c5a23ee63ad480fc1ad05d62d2f41ac3
B Kleinen <kleinen@infrastructure.de>
2017-07-11 09:20:53 +0200

    array permutator test

---
+++ test/assignment_02_permutation/ArrayPermutatorTest.java
@@ -0,0 +1,112 @@
+package assignment_02_permutation;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayPermutatorTest {
+
+	Random randomGenerator;
+
+	@Before
+	public void createSameRandomGeneratorForEachTest() {
+		randomGenerator = new Random(43);
+	}
+
+	@Test
+	public void testShuffleInts() {
+		ArrayPermutator<Integer> sut = new ArrayPermutator<>(randomGenerator);
+		Integer[] a = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		Integer[] original = Arrays.copyOf(a, a.length);
+
+		sut.shuffle(a);
+		
+		// there are two ways to implement the shuffle - starting from the start
+		// and starting from the beginning.
+		Integer[] expected_b = new Integer[] { 5, 8, 3, 1, 2, 6, 4, 10, 9, 7 };
+		Integer[] expected_f = new Integer[] { 7, 10, 9, 1, 2, 4, 6, 5, 3, 8};
+		
+		assertShuffle(a, original, expected_b, expected_f, "testShuffleInts");
+	}
+
+
+	@Test
+	public void testShuffleStrings() {
+		String[] a = "a b c d e f g h i j k l m n o p".split(" ");
+		ArrayPermutator<String> sut = new ArrayPermutator<>(randomGenerator);
+		String[] original = Arrays.copyOf(a, a.length);
+
+		sut.shuffle(a);
+		
+		String[] expected_f = "l, m, n, o, j, c, d, p, i, h, g, f, e, a, k, b".split(", ");
+		String[] expected_b = "b, m, h, d, c, e, g, a, j, k, i, f, n, o, p, l".split(", ");
+		
+		assertShuffle(a, original, expected_b, expected_f, "testShuffleStrings");
+	}
+
+	@Test
+	public void testOneElement() {
+		String[] a = { "a" };
+		ArrayPermutator<String> sut = new ArrayPermutator<>(randomGenerator);
+		
+		sut.shuffle(a);
+		
+		String[] expected = { "a" };
+		assertArrayEquals(expected, a);
+	}
+	@Test
+	public void testEmptyArray() {
+		String[] a = { };
+		ArrayPermutator<String> sut = new ArrayPermutator<>(randomGenerator);
+		
+		sut.shuffle(a);
+		
+		String[] expected = { };
+		assertArrayEquals(expected, a);
+	}
+
+	@Test
+	public void testTwoElements() {
+		ArrayPermutator<Integer> sut = new ArrayPermutator<>(randomGenerator);
+		Integer[] a = new Integer[] { 4, 7 };
+		Integer[] original = Arrays.copyOf(a, a.length);
+		
+		Integer[] expected_b = new Integer[] { 4, 7 };
+		Integer[] expected_f = new Integer[] { 7, 4 };
+	
+		sut.shuffle(a);
+		assertShuffle(a, original, expected_b, expected_f, "testTwoElements");
+
+	
+	}
+
+	
+
+	private void assertShuffle(Object[] a, Object[] original, Object[] expected_b, Object[] expected_f,
+			String methodName) {
+		String message = "";
+		boolean fail = false;
+
+		if (Arrays.equals(expected_f, a)) {
+			message += "You shuffled forward.";
+		} else if (Arrays.equals(expected_b, a)) {
+			message += "You shuffled backwards.";
+		} else if (Arrays.equals(original, a)) {
+			message += "Your array hasn't changed";
+			fail = true;
+		} else {
+			message += "Your array was changed, but not in the way the algorithm presented in class would have changed it";
+		}
+		System.out.println(methodName+":");
+		System.out.println("Original Array: "+Arrays.toString(original));
+		System.out.println("Shuffled Array: "+Arrays.toString(a));
+		System.out.println(message);
+		System.out.println();
+		
+		if (fail) fail(message);
+	}
+}
