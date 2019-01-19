a8854357e806cc46a6564cb5181fd800f91b75b7
Barne Kleinen <kleinen@infrastructure.de>
2019-01-19 13:16:28 +0100

    little improvements

--- src/assignment_01_list/Node.java
+++ src/assignment_01_list/Node.java
@@ -1,14 +1,19 @@
 package assignment_01_list;
 
-
 public class Node<E> {
 	   protected E data;
 	   protected Node<E> next;
 	  
 	   public Node (E data, Node<E> next) { 
-	     this.data = data; this.next = next;
+	     this.data = data; 
+	     this.next = next;
 	   }
-	   public Node (E data) { 
-	     this.data=data;
-	   }	  
+
+	public E getData() {
+		return data;
+	}
+
+	public Node<E> getNext() {
+		return next;
+	}
 }
--- src/assignment_02_permutation/ArrayPermutator.java
+++ src/assignment_02_permutation/ArrayPermutator.java
@@ -3,25 +3,34 @@ package assignment_02_permutation;
 import java.util.Random;
 
 /**
- * Assignment 02: Implement the method shuffle that
- * shuffles the array given as a parameter in place.
+ * Assignment 02: Implement the method shuffle that shuffles the array given as
+ * a parameter in place.
+ * 
+ * IMPORTANT: The tests pass in an instant of Random that is seeded with a fixed
+ * value. This way, the random values are always the same.
+ * 
  * @author kleinen
  *
  * @param <E>
  */
 public class ArrayPermutator<E> {
-	// don't create a new Instance of Random - use 
-	// the one passed by the test! 
-	Random random;
-	public ArrayPermutator(Random randomGenerator){
-		random = randomGenerator;
-	}
-	
+
 	/**
 	 * Shuffles the array in place.
+	 * 
 	 * @param a
 	 */
-	public void shuffle(E[] a){
-		// implement the method!
+	public void shuffle(E[] a) {
+		// implement the method.
+		// use the already available Random random;!
+	}
+
+	// nothing to do below this point - especially
+	// don't create a new Instance of Random - use
+	// the one passed by the test!
+	Random random;
+
+	public ArrayPermutator(Random randomGenerator) {
+		random = randomGenerator;
 	}
 }
